296cd9c6df285f460a69980d7600d6da9e603876
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-06 18:10:12 -0400

    Added MountainsViewController file

---
+++ MountainSearch/Controllers/MountainsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MountainsViewController.swift
+//  MountainSearch
+//
+//  Created by Marcus on 7/6/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class MountainsViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .purple
+	}
+}
