b8d18b5f036e3087ab95ff375afdfb0b95c1aa1c
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-06 19:14:18 -0400

    Added logic to calculateItemSize

--- MountainSearch/CollectionView/MountainsFlowLayout.swift
+++ MountainSearch/CollectionView/MountainsFlowLayout.swift
@@ -12,7 +12,7 @@ class MountainsFlowLayout: UICollectionViewFlowLayout {
 	private let scrollDir: UICollectionView.ScrollDirection = .vertical
 	private let minLineSpacing: CGFloat = 8.0
 	private let minInterItemSpacing: CGFloat = 8.0
-	private let sectInsets: UIEdgeInsets = UIEdgeInsets(top: 15.0, left: 0.0, bottom: 0.0, right: 0.0)
+	private let sectInsets: UIEdgeInsets = UIEdgeInsets(top: 15.0, left: 8.0, bottom: 0.0, right: 8.0)
 	
 	override func prepare() {
 		// configuration
@@ -20,5 +20,23 @@ class MountainsFlowLayout: UICollectionViewFlowLayout {
 		minimumLineSpacing = minLineSpacing
 		minimumInteritemSpacing = minInterItemSpacing
 		sectionInset = sectInsets
+		itemSize = calculateItemSize()
+	}
+	
+	private func calculateItemSize() -> CGSize {
+		// get full width
+		let collectionViewWidth = UIScreen.main.bounds.size.width
+
+		// subtract left & right margins
+		let fullWidthMinusHorizontalMargins = collectionViewWidth - (sectInsets.left + sectInsets.right)
+		
+		// subtract interItemSpacing
+		let widthMinusInterItemSpacing = fullWidthMinusHorizontalMargins - minInterItemSpacing
+		
+		// the objective is 2 items per row so divide by 2 to get width of individual item
+		let itemWidth = widthMinusInterItemSpacing / 2.0
+		
+		// dynamic width & static height
+		return CGSize(width: itemWidth, height: 44.0)
 	}
 }
