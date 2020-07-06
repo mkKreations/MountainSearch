efe7f1064c822d2d5211f4b0049f97213b73acec
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-06 11:55:39 -0400

    Added MountainsController object to generate Mountains

---
+++ MountainSearch/Models/MountainsController.swift
@@ -0,0 +1,33 @@
+//
+//  MountainsController.swift
+//  MountainSearch
+//
+//  Created by Marcus on 7/6/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import Foundation
+
+// this object will be responsible for
+// generating our Mountain models from
+// our Mountain raw data
+
+struct MountainsController {
+	// only generate mountains when we need them
+	lazy var mountains: [Mountain] = generateMountains()
+	
+	// no need to expose this logic
+	private func generateMountains() -> [Mountain] {
+		// parse thru Mountains by using newLine as separator
+		let components = mountainsRawData.components(separatedBy: CharacterSet.newlines)
+		var mountains = [Mountain]()
+		for line in components {
+			// separate [Mountain Name] from [Mountain Altitude]
+			let mountainComponents = line.components(separatedBy: ",")
+			let mountainName = mountainComponents[0]
+			let mountainAltitude = Int(mountainComponents[1])! // convert to Int
+			mountains.append(Mountain(name: mountainName, height: mountainAltitude))
+		}
+		return mountains
+	}
+}
