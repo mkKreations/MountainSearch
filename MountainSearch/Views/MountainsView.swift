80254b886259532a8804e9aea9b8303cacaf5fa0
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-06 18:16:58 -0400

    Added MountainsView with collectionView

---
+++ MountainSearch/Views/MountainsView.swift
@@ -0,0 +1,28 @@
+//
+//  MountainsView.swift
+//  MountainSearch
+//
+//  Created by Marcus on 7/6/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class MountainsView: UIView {
+	let collectionView: UICollectionView = UICollectionView()
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		collectionView.translatesAutoresizingMaskIntoConstraints = false
+		addSubview(collectionView)
+		
+		collectionView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+		collectionView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+		collectionView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+		collectionView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in MountainsView")
+	}
+}
