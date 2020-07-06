3858a1ce9f8a74d64833739d4dca57bdc0608df5
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-06 18:12:22 -0400

    MountainsViewController is rootVC - removed ViewController

--- MountainSearch/AppDelegate.swift
+++ MountainSearch/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		let rootVC = ViewController()
+		let rootVC = MountainsViewController()
 		let navVC = UINavigationController(rootViewController: rootVC)
 		window?.rootViewController = navVC
 		window?.makeKeyAndVisible()
