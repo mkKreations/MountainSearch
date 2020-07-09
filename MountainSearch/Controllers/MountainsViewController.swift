0ba033ae3664993d2db428a56b2cf80ea7469aa0
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-09 18:32:48 -0400

    Added controller dependency to viewController - SEE MORE
    Also generating our raw data upon initialization of view
    Controller

--- MountainSearch/Controllers/MountainsViewController.swift
+++ MountainSearch/Controllers/MountainsViewController.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class MountainsViewController: UIViewController {
+	// MARK: dependencies
+	var controller: MountainsController = MountainsController()
+
+	
+	// MARK: initializers
+	init() {
+		super.init(nibName: nil, bundle: nil) // have to call desginated init *eyeroll*
+		_ = controller.mountains // generate our mountains upon init
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in MountainsViewController")
+	}
+	
+	
+	// MARK: view lifecycle methods
 	override func loadView() {
 		view = MountainsView()
 	}
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		guard let myView = view as? MountainsView else {
-			return
-		}
+		guard let myView = view as? MountainsView else { return }
+		
 		myView.collectionView.backgroundColor = .red
 	}
-	
 }
