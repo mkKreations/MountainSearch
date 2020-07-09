6c6e90406b8e7a2f781ba60631934377c02ca23a
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-09 19:40:25 -0400

    Added styling to cell & some constraints - SEE MORE
    Cells are still not appearing

--- MountainSearch/Views/MountainCell.swift
+++ MountainSearch/Views/MountainCell.swift
@@ -22,12 +22,19 @@ class MountainsCell: UICollectionViewCell {
 		
 		contentView.backgroundColor = .darkBackgroundColor
 		
+		contentView.layer.borderColor = UIColor.darkGray.cgColor
+		contentView.layer.borderWidth = 1.0
+		
 		displayLabel = UILabel(frame: .zero)
 		displayLabel.translatesAutoresizingMaskIntoConstraints = false
+		displayLabel.textColor = .label
+		displayLabel.numberOfLines = 1
+		displayLabel.font = .systemFont(ofSize: 16)
 		contentView.addSubview(displayLabel)
 		
 		displayLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
 		displayLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8.0).isActive = true
+		displayLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 8.0).isActive = true
 	}
 	required init?(coder: NSCoder) {
 		fatalError("Crash in MountainsCell")
