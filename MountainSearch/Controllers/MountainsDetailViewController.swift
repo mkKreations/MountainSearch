d2d7888779f271ac88282a385ca72029bc4e81dc
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-09 11:59:15 -0400

    Added UITableViewDataSource conformance

--- MountainSearch/Controllers/MountainsDetailViewController.swift
+++ MountainSearch/Controllers/MountainsDetailViewController.swift
@@ -21,6 +21,24 @@ class MountainsDetailViewController: UIViewController {
 		
 		guard let detailView = view as? MountainsDetailView else { return }
 		
-		detailView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: String.init(describing: UITableViewCell.self))
+		detailView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
+	}
+}
+
+// this View Controller is so bare that I'll just add UITableViewDatasource conformance here
+
+extension MountainsDetailViewController: UITableViewDataSource {
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 2
+	}
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: UITableViewCell.self), for: indexPath)
+		guard let mount = mountain else { return cell }
+		cell.textLabel?.text = mount.name
+		cell.detailTextLabel?.text = "Altitude: \(mount.height)"
+		return cell
 	}
 }
