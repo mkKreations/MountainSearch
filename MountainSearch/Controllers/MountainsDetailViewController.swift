50ad7e153e8ac54cec9a5b2f23632bdbc69b5339
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-09 11:56:14 -0400

    Added nav title & registered cells in viewDidLoad

--- MountainSearch/Controllers/MountainsDetailViewController.swift
+++ MountainSearch/Controllers/MountainsDetailViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class MountainsDetailViewController: UIViewController {
+	var mountain: Mountain?
+	
 	override func loadView() {
 		view = MountainsDetailView()
 	}
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		title = mountain?.name
+		
+		guard let detailView = view as? MountainsDetailView else { return }
+		
+		detailView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: String.init(describing: UITableViewCell.self))
 	}
 }
