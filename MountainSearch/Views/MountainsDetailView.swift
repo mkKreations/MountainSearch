c7bb5bdcee8a6b0cf95ca9c31add1cca320989dd
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-09 11:38:57 -0400

    Added MountainsDetailView file

---
+++ MountainSearch/Views/MountainsDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MountainsDetailView.swift
+//  MountainSearch
+//
+//  Created by Marcus on 7/9/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class MountainsDetailView: UIView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in MountainsDetailView")
+	}
+}
