2ef7f7c430b9d2120f6daaf1787a22080f5e9353
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-10 18:36:45 -0400

    Added MountainsDetailView SwiftUI file

---
+++ MountainSearch/SwiftUI/MountainsDetailView.swift
@@ -0,0 +1,25 @@
+//
+//  MountainsDetailView.swift
+//  MountainSearch
+//
+//  Created by Marcus on 7/10/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import SwiftUI
+
+struct MountainsDetailView: View {
+	var body: some View {
+		Text("MountainsDetailView")
+	}
+}
+
+struct MountainsDetailView_Preview: PreviewProvider {
+	static var previews: some View {
+		Group {
+			MountainsDetailView()
+			MountainsDetailView()
+				.previewDevice("iPhone Xs Max")
+		}
+	}
+}
