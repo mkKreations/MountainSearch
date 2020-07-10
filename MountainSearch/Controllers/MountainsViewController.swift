9435b74805134e742ab88df7ba39470d818e9c1d
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-10 17:00:40 -0400

    Added section insets

--- MountainSearch/Controllers/MountainsViewController.swift
+++ MountainSearch/Controllers/MountainsViewController.swift
@@ -85,6 +85,7 @@ class MountainsViewController: UIViewController {
 		let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
 		
 		let section = NSCollectionLayoutSection(group: group)
+		section.contentInsets = NSDirectionalEdgeInsets(top: 25.0, leading: 8.0, bottom: 25.0, trailing: 8.0)
 		
 		return UICollectionViewCompositionalLayout(section: section)
 	}
