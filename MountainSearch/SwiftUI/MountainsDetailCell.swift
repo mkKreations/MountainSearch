248da6c9eb991a88064e103bdeb44dd378b9c887
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-10 21:39:45 -0400

    Added MountainsDetailCell file

---
+++ MountainSearch/SwiftUI/MountainsDetailCell.swift
@@ -0,0 +1,25 @@
+//
+//  MountainsDetailCell.swift
+//  MountainSearch
+//
+//  Created by Marcus on 7/10/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import SwiftUI
+
+struct MountainsDetailCell: View {
+	var body: some View {
+		Text("MountainsDetailCell")
+	}
+}
+
+struct MountainsDetailCell_Preview: PreviewProvider {
+	static var previews: some View {
+		Group {
+			MountainsDetailCell()
+			MountainsDetailCell()
+				.previewDevice("iPhone Xs Max")
+		}
+	}
+}
